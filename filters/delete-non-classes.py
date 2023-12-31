fcf6edcaa683d062765a351fa984013e777f41f4
Ruslan Galiullin <rmgaliullin@edu.hse.ru>
2023-12-31 07:15:33 +0000

    created statistics for 12repos Halstead volume, effort, and difficulty, CC, CoCo, NOBL

--- filters/delete-non-classes.py
+++ filters/delete-non-classes.py
@@ -20,78 +20,143 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+from timeit import default_timer as timer
 import sys
 import os
 import javalang
 import subprocess
 import time
+from multiprocessing import Pool, cpu_count
+import asyncio
+import aiofiles
 
 def collect_body(start, text):
     result = "{"
     balance = 1
+    backslash = 0
+    flag_double = False
     start += 1
-    while balance != 0:
-        if text[start] == "}":
+    while start < len(text) and balance != 0:
+        current_char = text[start]
+        prev_char = text[start - 1] if start > 0 else None
+        next_char = text[start + 1] if start + 1 < len(text) else None
+
+        is_double_quote = current_char == "\"" and backslash % 2 == 0 and not (prev_char == '\'' and next_char == '\'')
+        is_closing_brace = not flag_double and current_char == "}" and not (prev_char == '\'' and next_char == '\'')
+        is_opening_brace = not flag_double and current_char == "{" and not (prev_char == '\'' and next_char == '\'')
+        is_one_line_comment = not flag_double and (prev_char == '/' and current_char == '/')
+        is_several_lines_comment = not flag_double and (current_char == "/" and next_char == "*")
+
+        if is_double_quote:
+            flag_double = not flag_double
+        elif is_one_line_comment:
+            result += current_char
+            start = text.find('\n', start)
+            continue
+        elif is_several_lines_comment:
+            comment_end = text.find("*/", start) + 2
+            result += text[start:comment_end]
+            start = comment_end
+            continue
+        elif is_closing_brace:
             balance -= 1
-        if text[start] == "{":
+        elif is_opening_brace:
             balance += 1
-        result += text[start]
-        start += 1
+        result += current_char
         if balance == 0:
             break
+
+        start += 1
+        backslash = backslash + 1 if current_char == '\\' else 0
+
+    # if balance != 0:
+    #     raise RuntimeError(f"unparsable method")
     return result
 
-def find_methods_in_file(methods, text):
+def split_list(input_list, chunk_size):
+    return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
+
+async def find_methods_in_file(methods, text, dir_name):
     result_files = []
+    create_file_coroutines = []
     for index in range(len(methods)):
         pair = methods[index].split(":+:+:")
         if len(pair) != 3:
             continue
         class_name, method_name, return_type = pair[0], pair[1], pair[2]
+
         if len(method_name) == 0:
             continue
+
         if method_name[0] == "<":
             method_name = method_name.replace("<init>", class_name)
             return_type = ""
-        method_index = text.find(f" {method_name}")
+        method_index = text.find(f"{return_type} {method_name} ")
+        if method_index == -1:
+            continue
         start = text[method_index:].find("{")
-        if start != -1:
-            result_class_name = f"{class_name}_{method_name[:method_name.find('(')]}_{index}"
-            result_class_body = f"public {return_type} {method_name} {collect_body(start, text[method_index:])}"
-            result_files.append((result_class_name, f"class {result_class_name} {{{result_class_body}}}"))
+        if start == -1 or ";" in text[method_index:start]:
+            continue
+        result_class_name = f"{class_name}_{method_name[:method_name.find('(')]}_{index}"
+        result_class_body = f"public {return_type} {method_name} {collect_body(start, text[method_index:])}"
+
+        create_file_coroutine = create_file(result_class_name, f"class {result_class_name} {{{result_class_body}}}", dir_name)
+        create_file_coroutines.append(create_file_coroutine)
+
+    # Await all create_file coroutines concurrently
+    await asyncio.gather(*create_file_coroutines)
+
     return result_files
 
-def create_files(methods, dir):
-    for file, text in methods:
-        with open(os.path.join(dir, file + ".java"), 'w', encoding='utf-8') as others:
-            others.write(text)
+async def create_file(file, text, dir):
+    async with aiofiles.open(os.path.join(dir, file + ".java"), 'w', encoding='utf-8') as others:
+        await others.write(text)
+
+def run_async(methods, text, dir_name):
+    asyncio.run(find_methods_in_file(methods, text, dir_name))
 
 if __name__ == '__main__':
     JAVA: str = sys.argv[1]
     LST: str = sys.argv[2]
     try:
+        # start = timer()
         with open(JAVA, encoding='utf-8') as f:
-            text = f.read()
-            raw = javalang.parse.parse(text)
+            raw = javalang.parse.parse(f.read())
             tree = raw.filter(javalang.tree.ClassDeclaration)
             if not (tree := list((value for value in tree))):
                 os.remove(JAVA)
                 with open(LST, 'a+', encoding='utf-8') as others:
                     others.write(JAVA + "\n")
-            else:
-                # Compile the Java file
-                compile_command = "javac /cam/filters/java/org/example/Main.java"
-                subprocess.run(compile_command, shell=True, check=True)
-
-                # Execute the Java program
-                execute_command = f"java -cp /cam/filters/java org.example.Main {JAVA}"
-                result = subprocess.run(execute_command, shell=True, capture_output=True)
-                methods = find_methods_in_file(result.stdout.decode().split("\n"), text)
-                create_files(methods, os.path.dirname(JAVA))
-                with open(os.path.join(f"{os.path.dirname(LST)}/github/", JAVA), 'w', encoding='utf-8') as others:
-                    others.write(text)
-                os.remove(JAVA)
+                exit(0)
+                # with Pool() as pool:
+                    # Use starmap to pass multiple arguments to the function
+                    # res = pool.starmap(run_async, [(methods, text, dir_name) for methods in split_list(result, cpu_count())])
+                    # print(res)
+                # with open(os.path.join(f"{os.path.dirname(LST)}/github/", JAVA), 'w', encoding='utf-8') as others:
+                #     others.write(text)
+        # end = timer()
+        # print(end-start)
     except Exception as e:
-        with open("/cam/filters/error.out", 'w+', encoding='utf-8') as others:
-            others.write(f"Error {e.__str__()} occured in delete non classes files with file {JAVA} \n")
+        pass
+    try:
+        with open(JAVA, encoding='utf-8') as f:
+            text = f.read()
+            dir_name = os.path.dirname(JAVA)
+            # Compile the Java file
+            compile_command = "javac /cam/filters/java/org/example/Main.java" #/home/rmg
+            subprocess.run(compile_command, shell=True, check=True)
+
+            # Execute the Java program
+            execute_command = f"java -cp /cam/filters/java org.example.Main {JAVA}"
+            result = subprocess.run(execute_command, shell=True, capture_output=True)
+            result = result.stdout.decode().split("\n")
+
+            run_async(result, text, dir_name)
+    except Exception as e:
+        with open(LST, 'a+', encoding='utf-8') as others:
+            others.write(f"Error {JAVA}, e: {e}"+"\n")
+    
+    try:
+        os.remove(JAVA)
+    except Exception as e:
+        pass
